d5b1a3a3f1ed95da11297f12f8422b8e295c1da9
I-am-dinu <dkmm5590@gmail.com>
2018-10-24 12:36:18 +0530

    Project complete

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,8 +1,8 @@
 package com.londonappbrewery.magiceightball;
 
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,42 +18,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView ballDisplay;
-        final MediaPlayer mp;
+        final ImageView displayimg = findViewById(R.id.displayimg);
+        Button askButton = findViewById(R.id.ask);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.sparklingeffect);
 
 
-        ballDisplay = (ImageView) findViewById(R.id.displayBall);
-
-        final int[] ballArray = {
+        final int allImg[] = {
 
                 R.drawable.ball1,
                 R.drawable.ball2,
                 R.drawable.ball3,
                 R.drawable.ball4,
+                R.drawable.ball5
 
         };
 
-        Button myButton;
-
-        myButton = (Button) findViewById(R.id.askButton);
-        mp = MediaPlayer.create(getApplicationContext(),R.raw.bubblesound);
-
-
-        myButton.setOnClickListener(new View.OnClickListener() {
+        askButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mp.start();
-                Random randomNumber = new Random();
+                Random rg = new Random();
 
-                int number = randomNumber.nextInt(4);
+                int frg = rg.nextInt(5);
 
-                ballDisplay.setImageResource(ballArray[number]);
+                displayimg.setImageResource(allImg[frg]);
 
             }
         });
-
-
-
 
     }
 }
